302d9a3ca5f3d2173ea24b51790d89f1d0986b03
Ankit Jailwal <jailwalankit@gmail.com>
2022-04-08 03:56:21 +0530

    Added carousel and bottom nav bar

--- lib/screens/dashboard/components/body.dart
+++ lib/screens/dashboard/components/body.dart
@@ -1,4 +1,6 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import './BottomNavigationBar.dart';
 
 class body extends StatefulWidget {
   const body({Key? key}) : super(key: key);
@@ -8,10 +10,43 @@ class body extends StatefulWidget {
 }
 
 class _bodyState extends State<body> {
+  var height , width;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text("Dashboard Screen")
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Column(
+      children:  [
+        SizedBox(height: height*0.025),
+        Container(
+          height: height/4,
+          width: width,
+          child: Swiper(
+            itemBuilder: (BuildContext context, int index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  "https://www.hellokpop.com/wp-content/uploads/2018/09/asf_5.png",
+                  fit: BoxFit.fill,
+                ),
+              );
+            },
+            loop: true,
+            autoplay: true,
+            itemWidth: width*0.9,
+            layout: SwiperLayout.STACK,
+            itemCount: 10,
+            viewportFraction: 0.8,
+            scale: 0.9,
+          ),
+        ),
+        SizedBox(height: height*0.03),
+        Center(
+            child: Text("Dashboard Screen")
+        ),
+
+
+      ],
     );
   }
 }
