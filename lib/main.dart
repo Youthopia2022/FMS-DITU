35ab05ffb70b86db4aa0dfddaf8c36ee90042433
AyushSantri <ayushsantri222@gmail.com>
2022-04-07 22:58:11 +0530

    done with adding details in firestore

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'FMS DITU',
-      initialRoute: SignIn.routeName,
+      initialRoute: SignUp.routeName,
       routes: routes,
     );
   }
