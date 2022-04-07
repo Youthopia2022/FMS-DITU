28834f5e95f9673078369afe583af4b368034f2e
AyushSantri <ayushsantri222@gmail.com>
2022-04-07 14:27:42 +0530

    added firebase files and created a prototype UI for signup page

--- lib/screens/signup/components/body.dart
+++ lib/screens/signup/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -8,10 +9,104 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  late String _username;
+  late int _college = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text("SignUp Screen")
+    return Center(
+      child: Form(
+          child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            child: TextFormField(
+              keyboardType: TextInputType.name,
+              key: const ValueKey('username'),
+              style: const TextStyle(color: Colors.grey),
+              decoration: const InputDecoration(
+                labelText: "username",
+              ),
+              validator: (value) {
+                if (value?.isEmpty == true) {
+                  return "Incorrect name";
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _username = value!;
+              },
+            ),
+          ),
+          GroupButton(
+              buttons: const ["Male", "Female", "Other"],
+              onSelected: (i, selected) =>
+                  debugPrint("$i is selected $selected")),
+          GroupButton(
+              buttons: const ["DIT", "Other"],
+              onSelected: (i, selected) {
+                setState(() {
+                  _college = i;
+                });
+              }),
+          _college == 0
+              ? SizedBox(
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: TextFormField(
+                    keyboardType: TextInputType.name,
+                    key: const ValueKey('email'),
+                    style: const TextStyle(color: Colors.grey),
+                    decoration: const InputDecoration(
+                      labelText: "1000020104@dit.edu.in",
+                    ),
+                    onSaved: (value) {},
+                  ),
+                )
+              : SizedBox(
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: TextFormField(
+                    keyboardType: TextInputType.name,
+                    key: const ValueKey('email'),
+                    style: const TextStyle(color: Colors.grey),
+                    decoration: const InputDecoration(
+                      labelText: "john@gmail.com",
+                    ),
+                    onSaved: (value) {},
+                  ),
+                ),
+          GroupButton(
+            buttons: const ["1st year", "2nd year", "3rd year", "4th year"],
+            onSelected: (i, selected) => debugPrint("$i is selected $selected"),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            child: TextFormField(
+              keyboardType: TextInputType.name,
+              key: const ValueKey('password'),
+              style: const TextStyle(color: Colors.grey),
+              decoration: const InputDecoration(
+                labelText: "password",
+              ),
+              validator: (value) {},
+              onSaved: (value) {},
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2,
+            child: TextFormField(
+              keyboardType: TextInputType.name,
+              key: const ValueKey('re-enterPassword'),
+              style: const TextStyle(color: Colors.grey),
+              decoration: const InputDecoration(
+                labelText: "re-enter password",
+              ),
+              validator: (value) {},
+              onSaved: (value) {},
+            ),
+          ),
+          TextButton(onPressed: (){}, child: const Text("Submit")),
+        ],
+      )),
     );
   }
 }
