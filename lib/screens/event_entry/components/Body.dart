c71235084fe752bdff24a2c03723d92b3e8d12c3
AyushSantri <ayushsantri222@gmail.com>
2022-04-14 13:25:33 +0530

    added user permission

--- lib/screens/event_entry/components/Body.dart
+++ lib/screens/event_entry/components/Body.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fms_ditu/screens/event_entry/components/EventBrief.dart';
 import 'package:fms_ditu/screens/event_entry/components/EventShort.dart';
@@ -22,18 +23,15 @@ class Body extends StatelessWidget {
               child: Text("Add Event Info(Brief)"),
             ),
           ),
-          // Padding(
-          //   padding: const EdgeInsets.all(10),
-          //   child: ElevatedButton(
-          //     onPressed: () {
-          //       Navigator.push(
-          //         context,
-          //         MaterialPageRoute(builder: (context) => const EventShort()),
-          //       );
-          //     },
-          //     child: Text("Add Event Info(Short)"),
-          //   ),
-          // ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: ElevatedButton(
+              onPressed: () {
+               FirebaseAuth.instance.signOut();
+              },
+              child: Text("Sign OUt"),
+            ),
+          ),
         ],
       ),
     );
